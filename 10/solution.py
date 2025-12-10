e5b227b150386827535c4cd2a61d249a84f766b2
cmlimm <anton.motylev@proton.me>
2025-12-10 10:52:16 +0100

    solution for part 2 day 10

--- 10/solution.py
+++ 10/solution.py
@@ -1,8 +1,6 @@
 import os
 import pathlib
 
-from tqdm import tqdm
-
 
 def buttons_to_increases(list_buttons, max_n) -> list[int]:
     initial = [0] * max_n
@@ -52,8 +50,8 @@ for machine in machines:
             paths.append(path)
             break
 
-        neighbours = [(current ^ move, move) for move in moves]
-        for neighbour, move in neighbours:
+        neighbours = [current ^ move for move in moves]
+        for neighbour in neighbours:
             if not neighbour in visited:
                 queue.append((neighbour, path + 1))
                 visited.add(frozenset(neighbour))
@@ -62,33 +60,20 @@ print(sum(paths))
 
 # Part 2
 
-# paths = []
-# for machine in machines:
-#     print()
-#     print(machine)
+from scipy.optimize import linprog
 
-#     target_joltsages = machine[-1]
-#     max_n = len(target_joltsages)
-#     moves_j = [buttons_to_increases(buttons, max_n) for buttons in machine[1]]
+press_count = 0
+for machine in machines:
+    target_vector = machine[-1]
+    lights_count = len(machine[-1])
+    buttons = machine[1]
+    increases = [buttons_to_increases(button, lights_count) for button in buttons]
 
-#     visited_j = set()
-#     queue_j = []
-#     initial_j = [0] * max_n
-#     queue_j.append((initial_j, 0))
-#     visited_j.add(tuple(initial_j))
+    c = [1] * len(buttons)
+    a_eq = list(map(list, zip(*increases)))
+    b_eq = target_vector
 
-#     while queue_j:
-#         current_j, path_j = queue_j.pop(0)
-#         if current_j == target_joltsages:
-#             print(current_j, path_j)
-#             paths.append(path_j)
-#             break
+    result = linprog(c, A_eq=a_eq, b_eq=b_eq, integrality=1)
+    press_count += sum(result.x)
 
-#         neighbours_j = [([i + j for i, j in zip(current_j, move)], move) for move in moves_j]
-#         for neighbour, move in neighbours_j:
-#             t_neighbour = tuple(neighbour)
-#             if not t_neighbour in visited_j:
-#                 queue_j.append((neighbour, path_j + 1))
-#                 visited_j.add(t_neighbour)
-
-# print(sum(paths))
+print(int(press_count))
